6952e1436b782449960f6244545b92d1a3b4f40d
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2021-07-06 16:52:29 +1200

    need to sub divide

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -2,11 +2,9 @@
 namespace AdventureGame {
     class Program {
         static void Main() {
-            string CharacterName = "John Doe";
-            Console.WriteLine("Welcome to Adventure Game");
-            Console.WriteLine("What would you like your character's name to be?");
-            CharacterName = Console.ReadLine();
-            Console.WriteLine("Your character is now named " + CharacterName + "!");
+            Game g1 = new Game();
+            g1.Start();
+
             Console.ReadKey();
         }
     }
