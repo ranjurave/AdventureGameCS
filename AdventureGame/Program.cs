4407a07b5282bc50248f29dfd2d0aeb5f87a5817
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2021-07-06 14:32:00 +1200

    Initial commit

---
+++ AdventureGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AdventureGame {
+    class Program {
+        static void Main() {
+            string CharacterName = "John Doe";
+            Console.WriteLine("Welcome to Adventure Game");
+            Console.WriteLine("What would you like your character's name to be?");
+            CharacterName = Console.ReadLine();
+            Console.WriteLine("Your character is now named " + CharacterName + "!");
+            Console.ReadKey();
+        }
+    }
+}
